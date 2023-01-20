a036d3a5d7bcd12093fc7354a01344ee0d5cd4b3
razzh <razzhavenir@163.com>
2023-01-20 14:38:39 +0800

    chore: add NCSA4.0

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -4,7 +4,16 @@ import styles from '@/styles/footer.module.css'
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <p>Copyright © 2023-xiaohao</p>
+      <p>
+        <a
+          href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          CC BY-NC-SA 4.0
+        </a>{' '}
+        © Xiaohao
+      </p>
       <a className={textStyle.textLink} href="https://beian.miit.gov.cn/">
         浙ICP备17035458号-1
       </a>
