91d85fa25ea6b9362d43704860d474ec06ff2d3c
razzh <razzhavenir@163.com>
2023-03-14 21:00:26 +0800

    chore: 添加进度条

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import Layout from '@/components/Layout'
 import { AppProps } from 'next/app'
+import nProgress from 'nprogress'
+import Router from 'next/router'
 import '@/styles/global.css'
 import '@/styles/scrollbar.css'
 import '@/styles/markdown.css'
 import '@/styles/prose.css'
+import '@/styles/nprogress.css'
+
+Router.events.on('routeChangeStart', nProgress.start)
+Router.events.on('routeChangeError', nProgress.done)
+Router.events.on('routeChangeComplete', nProgress.done)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
