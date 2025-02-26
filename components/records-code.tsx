302b7038152eb226682292ad82d86ed5ef95fb35
razzh <razzhavenir@163.com>
2025-02-26 14:09:49 +0800

    style: 备案号颜色调整

--- components/records-code.tsx
+++ components/records-code.tsx
@@ -1,6 +1,6 @@
 export function RecordsCode() {
   return (
-    <div className="text-sm text-center">
+    <div className="text-muted text-sm text-center">
       <a href="https://beian.miit.gov.cn" target="_blank">
         浙ICP备2024129591号-1
       </a>
