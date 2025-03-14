28b1cadd5f4d9d251c34af67f063830570904d6d
razzh <razzhavenir@163.com>
2025-03-14 16:21:02 +0800

    fix: mobile tap delay

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -4,22 +4,32 @@ import Plum from './plum'
 import { ThemeProvider } from '@/components/theme-provider'
 import BackTop from '@/components/back-top'
 import { Analytics } from '@/components/analytics'
-import { type FC, type PropsWithChildren } from 'react'
+import { useEffect, type FC, type PropsWithChildren } from 'react'
 import AppProgressBar from '@/components/progress-bar'
 import { RecordsCode } from '@/components/records-code'
+import fastclick from 'fastclick'
 
-const Layout: FC<PropsWithChildren> = ({ children }) => (
-  <ThemeProvider>
-    <Navbar />
-    <Plum />
-    <main className="px-7 py-10 of-x-hidden">
-      {children}
-      <RecordsCode />
-    </main>
-    <BackTop />
-    <Analytics />
-    <AppProgressBar />
-  </ThemeProvider>
-)
+const Layout: FC<PropsWithChildren> = ({ children }) => {
+  useEffect(() => {
+    // @ts-expect-error
+    fastclick.attach(document.body)
+  })
+
+  return (
+    (
+      <ThemeProvider>
+        <Navbar />
+        <Plum />
+        <main className="px-7 py-10 of-x-hidden">
+          {children}
+          <RecordsCode />
+        </main>
+        <BackTop />
+        <Analytics />
+        <AppProgressBar />
+      </ThemeProvider>
+    )
+  )
+}
 
 export default Layout
