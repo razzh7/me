4a8070ba4c36945867c6530688b5e787ebf2bc2c
razzh <razzhavenir@163.com>
2025-03-17 18:29:14 +0800

    feat: add baidu analysis

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import '@/styles/global.css'
 import '@/styles/scrollbar.css'
 import '@/styles/mdx.css'
 import type { Metadata, Viewport } from 'next'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Razzh Blog',
@@ -33,6 +34,20 @@ export default function RootLayout({ children }: PropsWithChildren) {
           {children}
         </Layout>
       </body>
+      <Script
+        id='baidu-analysis'
+        strategy='lazyOnload'
+        dangerouslySetInnerHTML={{
+          __html: `var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?11038a882d198a857410c2ab295a2eff";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+        `
+        }}
+      />
     </html>
   )
 }
