fd08e3b2678397726ab71254e1d680ed3c7ba5c1
razzh <razzhavenir@163.com>
2025-04-28 18:44:46 +0800

    feat: add screen logo

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -7,16 +7,18 @@ import { Analytics } from '@/components/layout/vercel-analytics'
 import { type FC, type PropsWithChildren } from 'react'
 import AppProgressBar from '@/components/progress-bar'
 import { RecordsCode } from '@/components/records-code'
+import ScreenLogo from './screen-logo'
 
 const Layout: FC<PropsWithChildren> = ({ children }) => (
   (
     <ThemeProvider>
       <Navbar />
       <Plum />
-      <main className="px-7 py-10 of-x-hidden">
+      <main className="px-7 py-10 overflow-x-hidden">
         {children}
         <RecordsCode />
       </main>
+      <ScreenLogo />
       <BackTop />
       <Analytics />
       <AppProgressBar />
---
+++ components/layout/screen-logo.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import styles from '@/styles/screen-logo.module.css'
+import { useTheme } from '@/hooks/useTheme'
+import clsx from 'clsx'
+
+function ScreenLogo() {
+  const { theme } = useTheme()
+  const [showLogo, setShowLogo] = useState(true)
+  const [startAnimation, setStartAnimation] = useState(false)
+
+  useEffect(() => {
+    setStartAnimation(true)
+
+    const timer = setTimeout(() => {
+      setShowLogo(false)
+    }, 1500)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
+
+  return showLogo ? (
+    <div className={clsx(styles.logo, {
+      [styles['fade-out']]: startAnimation,
+      [styles['logo-dark']]: theme === 'dark',
+      [styles['logo-light']]: theme === 'light'
+    })}
+    >
+      <img src={theme === 'dark' ? '/razzh.svg' : '/razzh-drak.svg'} alt="screen-logo" />
+    </div>
+  ) : null
+}
+
+export default ScreenLogo
