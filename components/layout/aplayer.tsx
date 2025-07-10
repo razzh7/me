a0c0ad327ea34d2d91b650c5697c24096925c604
razzh <razzhavenir@163.com>
2025-07-10 18:31:19 +0800

    feat: update songs list

--- components/layout/aplayer.tsx
+++ components/layout/aplayer.tsx
@@ -8,6 +8,20 @@ function LayoutAplayer() {
   const isMobile = useIsMobile()
   const layoutAudioList: AudioInfo[] = useMemo(() => ([
     {
+      name: '戒网',
+      artist: '村西 / 王馨悦',
+      url: 'https://assets.razzh.cn/audio/jiewang.mp3',
+      cover: 'https://assets.razzh.cn/cover/jiewang.webp',
+      lrc: '[00:32.22]男：山海半生漂泊 一叶孤舟\n[00:44.95]女：天地两世零落 几处离愁\n[00:56.91]男：本是一处同飞鸟 余生无所求\n[01:04.48]女：空把青丝等白头\n[01:09.69]男：清泉无垢倒映着 我的心\n[01:16.5]女：恨到归时方始休\n[01:22.520004]男：为何心有灵犀 从来难长久\n[01:28.92]女：得见青天总在雨打风吹后\n[01:34.95]男：知我千万头绪烦忧\n[01:38.93]女：不知谓我何求\n[01:42.21]男：可否执子之手\n[01:45.32]女：来抓住我的手\n[01:48.240005]男：为何相见恨晚 情深却不寿\n[01:54.64]女：管他虚情假意花落水自流\n[02:00.63]男：知我千万头绪烦忧\n[02:04.51]女：不知谓我何求\n[02:07.74]合：待尽扫西风去 乌有\n[02:40.1]男：踏破银河无觅 玉颜红袖\n[02:52.91]女：寻遍宫阙不见 良人佳偶\n[03:04.94]男：一夜雨声多少事 月下强说愁\n[03:12.5]女：红豆如何敢消受\n[03:17.72]男：功名休问几时成 今朝酒\n[03:24.51]女：更无一个肯回头\n[03:30.5]男：纵然意合情投 怎能长相守\n[03:36.86]女：如醉方醒原来情钟不自由\n[03:42.95999]男：知我千万头绪烦忧\n[03:46.92]女：不知谓我何求\n[03:50.15]男：世事不如山丘\n[03:53.31]女：山青不如水秀\n[03:56.14]男：为何相见恨晚 情深却不寿\n[04:02.5]女：管他虚情假意花落水自流\n[04:08.53]男：知我千万头绪烦忧\n[04:12.46]女：不知谓我何求\n[04:15.7]合：待尽扫西风去 乌有\n[04:43.82]男：独立小桥等风满袖\n[04:47.74]女：去年此门依旧\n[04:50.99]合：夜灯为君留 归来否\n'
+    },
+    {
+      name: '想念',
+      artist: '梁博',
+      url: 'https://assets.razzh.cn/audio/xiangnian.mp3',
+      cover: 'https://p1.music.126.net/5-xvSZZxcQgvvk7sJ0-xPg==/109951169775167100.jpg?param=130y130',
+      lrc: '[00:37.02]其实孤独从来都和那些节日无关\n[00:42.38]像外面曾与我无关\n[00:48.25]就像曾经熟悉的那些单纯的笑脸\n[00:53.55]现在也和你无关\n[00:58.96]我们都相遇在这里的同一个地点\n[01:04.46]又分别在同一时间\n[01:09.89]那时候都说好要记住你的诺言\n[01:15.25]你的诺言不变\n[01:20.2]想念昨天的风\n[01:22.47]也想念昨天的雨\n[01:25.31]也想念昨天的黄昏和海岸\n[01:31.01]想念那时的你\n[01:33.61]也想念那时的我\n[01:36.18]也想念那时的一切都简单\n[01:41.92]想念冬天的雪\n[01:44.4]也想念家人的暖\n[01:47.11]也想念离开时的那个夜晚\n[01:52.75]我在想念\n[01:54.770004]是否你也在想念\n[01:57.479996]是否也远在天边\n[02:00.31]默默地祝愿\n[02:04.43]有些快乐就定格在离开家的那天\n[02:09.66]那是个永恒的节点\n[02:15.45]从此风雨兼程地走用心地追赶\n[02:20.62]可是彩虹永远在天边\n[02:26.34]因为相信也因为不信所以去冒险\n[02:31.6]看看一切的另一面\n[02:37.22]难过时候犹豫的我就看到你的脸\n[02:42.3]出现在我心里面\n[02:47.35]想念家乡的风\n[02:49.85]也想念家乡的路\n[02:52.52]也想念家乡的黄昏和海岸\n[02:58.29001]想念那时的你\n[03:00.74]也想念那时的我\n[03:03.44]也想念那时的一切都简单\n[03:09.24]想念冬天的雪\n[03:11.64]也想念家人的暖\n[03:14.3]也想念离开时的那个夜晚\n[03:20.14]我在想念\n[03:22.01]是否你也在想念\n[03:24.72]是否也远在天边\n[03:27.52]默默地祝愿\n[03:52.91]想念家乡的风\n[03:55.19]也想念家乡的雨\n[03:58.0]也想念家乡的黄昏和海岸\n[04:03.79]想念那时的你\n[04:06.13]也想念那时的我\n[04:08.81]也想念那时的一切都简单\n[04:14.68]想念冬天的雪\n[04:17.07]也想念家人的暖\n[04:19.75]也想念离开时的那个夜晚\n[04:25.65]我在想念\n[04:27.5]是否你也在想念\n[04:30.23]是否也远在天边\n[04:32.99]默默地祝愿\n[04:59.04]想家 想家 为什么想家\n[05:01.87]不说 不说 你也知道吗\n[05:04.55]想她 想她 为什么想她\n[05:07.18]Na na na na na na na na\n[05:09.86]想家 想家 为什么想家\n[05:12.73]不说 不说 你也知道吗\n[05:15.41]想她 想她 为什么想她\n[05:18.06]Na na na na na na na na\n[05:20.79]想家 想家 为什么想家\n[05:23.59]不说 不说 你也知道吗\n[05:26.28]想她 想她 为什么想她\n[05:28.98]Na na na na na na na na\n[05:31.79]想家 想家 为什么想家\n[05:34.53]不说 不说 你也知道吗\n[05:37.13]想她 想她\n[05:38.8]Na na na na na na na na\n'
+    },
+    {
       name: 'Special One',
       artist: 'AGA / Eason',
       url: 'https://assets.razzh.cn/audio/special-one.mp3',
@@ -38,6 +52,20 @@ function LayoutAplayer() {
       url: 'https://assets.razzh.cn/audio/hotel-california.mp3',
       cover: 'https://p1.music.126.net/aEMzfmJT4S46Jw4dTRoHGA==/109951167661766933.jpg?param=130y130',
       lrc: `[02:08.93]On a dark desert highway cool wind in my hair\n[02:15.37]Warm smell of colitas rising up through the air\n[02:21.74]Up ahead in the distance I saw a shimmering light\n[02:28.16]My head grew heavy and my sight grew dim\n[02:31.38]I had to stop for the night\n[02:34.59]There she stood in the doorway\n[02:37.69]I heard the mission bell\n[02:40.94]And I was thinking to myself\n[02:43.34]This could be Heaven or this could be Hell\n[02:47.40]Then she lit up a candle and she showed me the way\n[02:53.77]There were voices down the corridor\n[02:57.01]I thought I heard them say\n[03:00.07]Welcome to the Hotel California\n[03:05.79]Such a lovely place\n[03:07.66]Such a lovely face\n[03:08.97]Such a lovely face\n[03:12.53]Plenty of rooms at the Hotel California\n[03:18.69]Any time of year you can find it here\n[03:25.75]Her mind is Tiffany- twisted\n[03:28.96]She got the Mercedes Benz\n[03:32.20]She got a lot of pretty pretty boys\n[03:35.44]That she calls friends\n[03:38.57]How they dance in the courtyard\n[03:41.44]Sweet summer sweat\n[03:44.87]Some dance to remember\n[03:48.10]Some dance to forget\n[03:51.32]So I called up the Captain\n[03:54.55]Please bring me my wine\n[03:57.50]He said\n[03:58.27]We haven't had that spirit here since nineteen sixty nine\n[04:04.11]And still those voices are calling from far away\n[04:10.55]Wake you up in the middle of the night\n[04:13.44]Just to hear them say\n[04:16.94]Welcome to the Hotel California\n[04:22.55]Such a lovely place\n[04:24.32]Such a lovely place\n[04:25.92]Such a lovely face\n[04:29.26]They living it up at the Hotel California\n[04:35.39]What nice surprise\n[04:37.03]What nice surprise\n[04:38.77]Bring your alibis\n[04:42.96]Mirrors on the ceiling\n[04:45.98]Pink champagne on ice\n[04:48.51]She said\n[04:49.44]We are all just prisoners here\n[04:52.67]Of our own device\n[04:55.89]In the master's chambers\n[04:59.27]They gathered for the feast\n[05:02.45]They stabbed it with their steely knives\n[05:05.30]But they just can't kill the beast\n[05:08.97]Last thing I remember\n[05:11.69]I was running for the door\n[05:15.46]I had to find the passage Back\n[05:17.76]To the place I was before\n[05:22.09]Relax said the night man\n[05:24.60]We are programmed to receive\n[05:28.28]You can check out any time you like\n[05:31.60]But you can never leave\n`
+    },
+    {
+      name: '岁月如歌',
+      artist: '陈奕迅',
+      url: 'https://assets.razzh.cn/audio/suiyueruge.mp3',
+      cover: 'https://p1.music.126.net/c9G5m3D3GQxqbTt6MqJIgw==/18956679974612526.jpg?param=130y130',
+      lrc: '[00:14.36]爱上了 看见你\n[00:15.88]如何不懂谦卑\n[00:17.78]去讲心中理想 不会俗气\n[00:22.75]犹如看得见晨曦\n[00:26.14]才能欢天喜地\n[00:28.02]抱着你 我每次\n[00:29.74]回来多少惊喜\n[00:31.45]也许一生太短 陪着你\n[00:36.47]情感有若行李\n[00:39.86]仍然沉重待我整理\n[00:42.85]天气不似预期\n[00:45.94]但要走 总要飞\n[00:49.77]道别不可再等你\n[00:53.67]不管有没有机\n[00:56.71]给我体贴入微\n[00:59.6]但你手 如明日便要远离\n[01:04.37]愿你可以\n[01:06.15]留下共我曾愉快的忆记\n[01:11.520004]当世事再没完美\n[01:14.91]可远在岁月如歌中找你\n[01:33.130005]再见了 背向你\n[01:34.71]眉头多少伤悲\n[01:36.43]也许不必再讲 所有道理\n[01:41.58]何时放松我自己\n[01:45.130005]才能花天酒地\n[01:46.96]抱着你 我说过\n[01:48.380005]如何一起高飞\n[01:50.1]这天只想带走 还是你\n[01:55.39]如重温往日邮寄\n[01:58.65]但会否疲倦了嬉戏\n[02:25.99]天气不似预期\n[02:28.72]但要走 总要飞\n[02:32.57]道别不可再等你\n[02:36.13]不管有没有机\n[02:39.48]给我体贴入微\n[02:42.42]但你手 如明日便要远离\n[02:47.18]愿你可以\n[02:48.8]留下共我曾愉快的忆记\n[02:54.47]当世事再没完美\n[02:57.76]可远在岁月如歌中找你\n'
+    },
+    {
+      name: '黄金时代',
+      artist: '陈奕迅',
+      url: 'https://assets.razzh.cn/audio/golden-era.mp3',
+      cover: 'https://p1.music.126.net/LLwcJY9QWi0S2UDthkmn5A==/109951168369175665.jpg?param=130y130',
+      lrc: '[00:13.74]买了球鞋再买玩具\n[00:18.97]甚至想\n[00:22.31]花光一切买新居\n[00:27.04]爱上谈情再爱入睡\n[00:32.24]直到想\n[00:35.37]躺进陌生者的家里\n[00:40.5]你我永远不肯定爱不爱谁\n[00:46.56]约不约定谁\n[00:50.09]黄金广场内分手\n[00:53.01]在时代门外再聚\n[00:56.7]你和谁结伴前来\n[01:00.0]是否比我精采\n[01:03.37]自从前爱到现在\n[01:06.37]是哪个可一可再\n[01:10.09]你回来你不回来\n[01:13.270004]尽管天蹋下来\n[01:16.67]但仍然值得与你\n[01:19.729996]用余下时间谈论爱\n[01:37.08]吃喝完成再去玩乐\n[01:42.56]甚至想\n[01:45.770004]天光之际看星光\n[01:50.43]吻你眉头吻至寂寞\n[01:55.520004]直到想\n[01:59.14]拥吻漫画中的主角\n[02:03.76]你我永远不肯定爱不爱谁\n[02:10.12]约不约定谁\n[02:13.64]黄金广场内分手\n[02:16.43]在时代门外再聚\n[02:19.93]你和谁结伴前来\n[02:23.4]是否比我精采\n[02:26.70999]自从前爱到现在\n[02:29.72]是哪个可一可再\n[02:33.28]你回来你不回来\n[02:36.70999]尽管天蹋下来\n[02:40.18]但仍然值得与你\n[02:43.05]没错过什么再分开\n[03:00.7]你与我凑巧经过\n[03:03.42]就像在咖啡座\n[03:06.44]一个两个三个\n[03:09.41]太闷或是太多\n[03:16.45999]你和谁结伴前来\n[03:19.87]是否比我精采\n[03:23.4]自从前爱到现在\n[03:26.44]是哪个可一可再\n[03:30.02]你回来你不回来\n[03:33.13]尽管天蹋下来\n[03:36.73]但仍然值得与你\n[03:39.69]没错过什么再分开\n'
     }
   ]), [])
 
@@ -47,7 +75,6 @@ function LayoutAplayer() {
         audio={layoutAudioList}
         appearance='fixed'
         mini
-        listFolded
         mutex
       />
     ) : null
