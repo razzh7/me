acd39f858d6d4f9bf3cb131d99876f43cf99321f
razzh <razzhavenir@163.com>
2026-01-01 16:00:48 +0800

    fix(post): fix copy btn not work

--- components/mdx-pre.tsx
+++ components/mdx-pre.tsx
@@ -4,12 +4,13 @@ import CopyButton from '@/components/copy-button'
 import { cn } from '@/util/merge'
 
 interface MdxPreProps extends React.HTMLAttributes<HTMLPreElement> {
-  __rawString__?: string,
+  'data-raw-string'?: string,
   'data-language'?: string
 }
 
 
-function MdxPre({ __rawString__, ...props }: MdxPreProps) {
+function MdxPre(props: MdxPreProps) {
+  const rawString = props['data-raw-string']
   const lang = useMemo(() => {
     const rawLang = props['data-language']
     if (rawLang) {
@@ -17,14 +18,14 @@ function MdxPre({ __rawString__, ...props }: MdxPreProps) {
     }
     return ''
   }, [props])
-
+  console.log('data-raw-string', rawString)
   return (
     <div
       className="relative bg-code rounded-lg border border-muted2"
     >
       <div className="flex justify-between border-b border-muted2 px-[1rem] py-[0.3rem]">
         <div>{lang}</div>
-        <CopyButton value={__rawString__ as string} />
+        <CopyButton value={rawString as string} />
       </div>
       <pre className={cn('max-h-[650px] overflow-x-auto p-4')} {...props} />
     </div>
--- velite.config.ts
+++ velite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, s } from 'velite'
 import rehypePrettyCode from 'rehype-pretty-code'
 import rehypeSlug from 'rehype-slug'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import { rehypePreRaw, rehypePostRaw } from '@/util/rehype-raw'
 import process from 'process'
 
 const cwd = process.cwd()
@@ -35,9 +34,18 @@ export default defineConfig({
     gfm: true,
     rehypePlugins: [
       rehypeSlug,
-      rehypePreRaw,
-      [rehypePrettyCode, { theme: 'vitesse-dark' }],
-      rehypePostRaw,
+      [rehypePrettyCode, {
+        theme: 'vitesse-dark',
+        transformers: [
+          {
+            name: 'preserve-raw-string',
+            // 在代码块处理时，将原始代码存储到 pre 元素的属性上
+            pre(node: any) {
+              node.properties['data-raw-string'] = (this as any).source
+            }
+          }
+        ]
+      }],
       [rehypeAutolinkHeadings, {
         properties: {
           className: ["subheading-anchor"],
